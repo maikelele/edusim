f15ae7172c4a588fe1dac83944417fa60bb24c56
maikelele <lahcim.md@gmail.com>
2024-12-25 23:55:52 +0100

    added getPlots route

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authRoutes = express.Router();
 const {loginUser , registerUser } = require('./controllers/authController');
-const { savePlot } = require('./controllers/plotController');
+const { savePlot, getPlots } = require('./controllers/plotController');
 
 authRoutes.post('/login', loginUser);
 authRoutes.post('/register', registerUser);
 authRoutes.post('/savePlot', savePlot);
+authRoutes.post('/getPlots', getPlots);
 
 module.exports = authRoutes;
